a55bd6399ebc65aa722c9a12dfdbde03bc54c4f2
Marcel Huber <marcelhuberfoo@gmail.com>
2010-10-23 16:05:57 +0200

    replaced WD_ prefix with COAST_

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # batch file to start cookbook service
 
 export WWW_DIR=$DEV_HOME/WWW
-export WD_ROOT=$WWW_DIR/recipes
-export WD_PATH=.:config:config/HTMLTemplates
+export COAST_ROOT=$WWW_DIR/recipes
+export COAST_PATH=.:config:config/HTMLTemplates
 
 $WWW_DIR/wdapp/wdapp AppId=recipes
