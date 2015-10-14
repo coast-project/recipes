d236318ae8cff5d2c3c5783fc4667a055175aa6d
Marcel Huber <marcelhuberfoo@gmail.com>
2015-10-14 07:37:33 +0200

    build: added CoastActions dependency
    Change-Id: Ib662469b50df734052ac1214e7d3d2ea3ab4dcc9

--- config/prjconfig.sh
+++ config/prjconfig.sh
@@ -1,17 +1,27 @@
-#--------------------------------------------------------------------
-# Copyright (c) 2010, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
+#-----------------------------------------------------------------------------------------------------
+# Copyright (c) 2005, Peter Sommerlad and IFS Institute for Software at HSR Rapperswil, Switzerland
 # All rights reserved.
 #
 # This library/application is free software; you can redistribute and/or modify it under the terms of
 # the license that is included with this library/application in the file license.txt.
-#--------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+#
+# project specific variables and directories, loaded from within config.sh
+#
+
+############################################################################
+#
+# In this section you should only adjust but NOT remove any of the given variables
+# because these are used from within other scripts
+#
 
 # overwrite this one ONLY if the LOGDIR variable points to the wrong place
 LOGDIR=##LOGDIRREL##
 
+APP_NAME=##APP_NAME##
 SERVERNAME=##SERVERNAME##
 #PRJ_DESCRIPTION="Some Server"
-#PID_FILE=$PROJECTDIR/$LOGDIR/$SERVERNAME.PID
+#PID_FILE=$LOGDIRABS/$SERVERNAME.PID
 
 # RUN_USER variable will be used by bootScript.sh to start the server as a different user at boot time or later
 # -> this setting is needed to control the server (start|stop|restart...) by a user different than root
@@ -26,11 +36,7 @@ RUN_USER=##RUNUSERNAME##
 # if the RUN_SERVICE flag is left off or set to 1, the server will always be controllable using any of the bootScript.sh, startwd[as].sh, startprf.sh or stopwds.sh scripts
 # if RUN_SERVICE is set to 0, the server will not be started/stopped except the -F option is given to override the variable
 #RUN_SERVICE_CFGFILE=${DEV_HOME}/my_services.sh
-#RUN_SERVICE=`/bin/ksh -c ". ${RUN_SERVICE_CFGFILE} >/dev/null 2>&1; eval \"echo $\"RUN_SERVICE_${SERVERNAME}"`
-
-# name of the tared and gzipped distribution file, caution: try to use short names
-# because some CD burning software do only store 8.3 names
-TARGZNAME=$SERVERNAME.tgz
+#RUN_SERVICE=`/bin/sh -c ". ${RUN_SERVICE_CFGFILE} >/dev/null 2>&1; eval \"echo $\"RUN_SERVICE_${SERVERNAME}"`
 
 # overwrite this one ONLY if the COAST_PATH variable points to the wrong place
 COAST_PATH=##COAST_PATH##
@@ -41,11 +47,26 @@ COAST_PATH=##COAST_PATH##
 # note: Memory mapped files will always increase the file size by an internally managed blocksize,
 #  on SunOS_5.8, this blocksize seems to be 8192 bytes. If you intend to use a tail -f on these files
 #  you will probably not get what you expect. tail can not handle the reserved - and still unused - space.
-#export COAST_USE_MMAP_STREAMS=0
+#COAST_USE_MMAP_STREAMS=0
 
-# The flag TRACE_STORAGE defines the logging level of memory statistics
+# The flag COAST_TRACE_STORAGE defines the logging level of memory statistics
 #  0: No pool statistic tracing, except when excess memory was used
 #  1: Trace overall statistics
 #  2: Trace detailed statistics
 #  3: Trace unfreed blocks
-#export TRACE_STORAGE=0
+#COAST_TRACE_STORAGE=0
+
+# COAST_DOLOG controls the level of severities shown in the syslog. See below for possible values.
+# 1: DEBUG
+# 2: INFO
+# 3: WARNING
+# 4: ERROR
+# 5: ALERT
+# All messages with a severity above or equal the specified value will log onto the appropriate channel.
+#COAST_DOLOG=5
+
+# COAST_LOGONCERR controls the level of severities shown on the console. See above for possible values.
+#COAST_LOGONCERR=4
+
+#Enabling COAST_LOGONCERR_WITH_TIMESTAMP will prepend a timestamp to all messages logged to the console.
+#COAST_LOGONCERR_WITH_TIMESTAMP=1
